7d0d52be5b452da25ab123d997ee8c3c1033eeed
JLMSC <39315631+ReeCaptcha@users.noreply.github.com>
2019-08-11 15:19:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,91 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const token = process.env.token;
+const token = "NjEwMTAwMzIxOTA5MzQyMjA5.XVAxTQ.EwTfY3a1IcNjURQbwVARgwfLl2Y";//process.env.token;
+const fs = require("fs");
+const readline = require("readline");
+
+var result;
 
 client.on("ready", () => {
-  console.log("Bot foi iniciado.")
+  console.log("Bot online.")
 });
 
 client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type == "dm") return;
+  
+  const prefixes = [config.prefix[0], config.prefix[1], config.prefix[2], config.prefix[3]];
+  let prefix = false;
+  for (const thisPrefix of prefixes) {
+    if (message.content.startsWith(thisPrefix)) prefix = thisPrefix;
+  }
+  if (!prefix) return;
+  
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0]
+  let args = messageArray.slice(1);
+  let comando = cmd.slice(prefix.length).toLowerCase();
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const comando = args.shift().toLowerCase();
 
   if (comando === "meme") {
-    return await message.channel.send('r')
+    var data;
+    fs.readFile('./meme.txt', 'utf8', function (err, rawData) {
+      if (err) return console.log(err);
+      data = rawData.split('\n');
+      
+      function randomInt(l, h) {
+        return Math.floor(Math.random() * (h - l) + l);
+      }
+
+      resultChange(data[randomInt(0, data.length)]);
+    });
+
+    async function resultChange(input) {
+      result = input
+      const embed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setImage(result)
+      return await message.channel.send(embed);
+    }
+  }
+
+  if (comando === "ben") {
+    var data;
+    fs.readFile('./ben.txt', 'utf8', function (err, rawData) {
+      if (err) return console.log(err);
+      data = rawData.split('\n');
+      
+      function randomInt(l, h) {
+        return Math.floor(Math.random() * (h - l) + l);
+      }
+
+      resultChange(data[randomInt(0, data.length)]);
+    });
+
+    async function resultChange(input) {
+      result = input
+      return await message.channel.send(result);
+    }
+  }
+
+  if (comando == "phypo") {
+    var data;
+    fs.readFile('./phypo.txt', 'utf8', function (err, rawData) {
+      if (err) return console.log(err);
+      data = rawData.split('\n');
+      
+      function randomInt(l, h) {
+        return Math.floor(Math.random() * (h - l) + l);
+      }
+
+      resultChange(data[randomInt(0, data.length)]);
+    });
+
+    async function resultChange(input) {
+      result = input
+      return await message.channel.send(result);
+    }
   }
 });
 
